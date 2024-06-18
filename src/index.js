700bac761864c19cd24249d4f3c2150524318568
RaffleX <rafflexdevs@gmail.com>
2024-06-19 01:37:49 +0530

    fixes asper react 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./tailwind.css";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
     <App />
-  </ThirdwebProvider>,
-  document.getElementById("root")
+  </ThirdwebProvider>
 );
